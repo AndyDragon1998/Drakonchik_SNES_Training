a3d8991dd913be8406cd2e3a84ae31ba37b48549
Drakonchik <62643332+AndyDragon1998@users.noreply.github.com>
2024-07-31 01:13:52 +0300

    Intro Image Test
    Trying to set up engine with splash screen of sorts

--- Data/introState.asm
+++ Data/introState.asm
@@ -21,30 +21,95 @@ introStart:
 	
 	; A8 for 8 bit numbers and A16 for 16 bit adresses I guess...?
 	A16		
+	; DMA from BG_Palette to CGRAM
 	stz CGADD 		; $2121 set color address to 0
 	lda #$001f	 	; palette low byte gggrrrrr
-				 	; 1f = all the red bits
-	sta CGDATA 		; $2122 - write twice
-	lda #$00	 	; palette high byte -bbbbbgg
-	sta CGDATA 		; $2122
+	stz $4300 ; transfer mode 0 = 1 register write once
+	lda #$22  ; $2122
+	sta $4301 ; destination, cgram data
+	ldx #.loword(BG_Palette)
+	stx $4302 ; source
+	lda #^BG_Palette
+	sta $4304 ; bank
+	ldx #32
+	stx $4305 ; length
+	lda #1
+	sta MDMAEN ; $420b start dma, channel 0
+	
+	; DMA from Tiles to VRAM	
+	lda #V_INC_1 ; the value $80
+	sta VMAIN  ; $2115 = set the increment mode +1
+	ldx #$0000
+	stx VMADDL ; $2116 set an address in the vram of $0000
+	
+	lda #1
+	sta $4300 ; transfer mode, 2 registers 1 write
+			  ; $2118 and $2119 are a pair Low/High
+	lda #$18  ; $2118
+	sta $4301 ; destination, vram data
+	ldx #.loword(Tiles)
+	stx $4302 ; source
+	lda #^Tiles
+	sta $4304 ; bank
+	ldx #(End_Tiles-Tiles)
+	stx $4305 ; length
+	lda #1
+	sta MDMAEN ; $420b start dma, channel 0
+	
+	
+	; DMA from Tilemap to VRAM	
+	ldx #$6000
+	stx VMADDL ; $2116 set an address in the vram of $6000
+	
+	lda #1
+	sta $4300 ; transfer mode, 2 registers 1 write
+			  ; $2118 and $2119 are a pair Low/High
+	lda #$18  ; $2118
+	sta $4301 ; destination, vram data
+	ldx #.loword(Tilemap)
+	stx $4302 ; source
+	lda #^Tilemap
+	sta $4304 ; bank
+	ldx #$700
+	stx $4305 ; length
+	lda #1
+	sta MDMAEN ; $420b start dma, channel 0	
+	
+	
+	; a is still 8 bit.
+	lda #1 ; mode 1, tilesize 8x8 all
+	sta BGMODE ; $2105
+	
+	; 210b = tilesets for bg 1 and bg 2
+	; (210c for bg 3 and bg 4)
+	; steps of $1000 -321-321... bg2 bg1
+	stz BG12NBA ; $210b BG 1 and 2 TILES at VRAM address $0000
+	
+	; 2107 map address bg 1, steps of $400... -54321yx
+	; y/x = map size... 0,0 = 32x32 tiles
+	; $6000 / $100 = $60
+	lda #$60 ; bg1 map at VRAM address $6000
+	sta BG1SC ; $2107
 
-	; -bbbbbgg gggrrrrr
-	; black = $0000
-	; red = $001f
-	; green = $03e0
-	; blue = $7c00
-	; white = $7fff
-
-	; turn the screen on (end forced blank)
-	lda #$0f ; $0f
+	lda #BG1_ON	; $01 = only bg 1 is active
+	sta TM ; $212c
+	
+	lda #$0f ; $0f = turn the screen on (end forced blank)
 	sta INIDISP ; $2100
 
 rts
 
 introUpdate:
-	lda #$0002
-	sta newState
-	jsr stateMachineChange
+	XY16
+	;should work fine regardless of size of A
+		lda in_nmi ;load A register with previous in_nmi
+	@check_again:	
+		WAI ;wait for an interrupt
+		cmp in_nmi	;compare A to current in_nmi
+					;wait for it to change
+					;make sure it was an nmi interrupt
+		beq @check_again
+		rts
 rts
 
 introDraw:
@@ -52,6 +117,23 @@ introDraw:
 rts
 
 introEnd:
-	stz CGADD
-	stz CGDATA
+
 rts
+
+; Include all the image data used for Menu Screen
+
+.segment "RODATA"
+
+BG_Palette:
+; 32 bytes
+.incbin "../Graphics/Palettes/gameJam.pal"
+
+Tiles:
+; 4bpp tileset
+.incbin "../Graphics/Tiles/gameJam.chr"
+End_Tiles:
+
+Tilemap:
+; $700 bytes
+.incbin "../Graphics/Maps/gameJam.map"
+
--- Makefile
+++ Makefile
@@ -5,13 +5,14 @@ MODE=--pause-mode=always
 INC= Data/
 CNFG= Data/Config/lorom256k.cfg
 LABEL=Data/Config/labels.txt
+DEBUG=Data/Debug/debugOutput.txt
 
 SRC = main.asm
 BIN = main.o
 
 all: $(SRC)
 	$(ASM) $(SRC) -g
-	$(LINKER) -C $(CNFG) -o $(NAME) $(BIN) -Ln $(LABEL)
+	$(LINKER) -C $(CNFG) -o $(NAME) $(BIN) -Ln $(LABEL) --dbgfile $(DEBUG)
 	
 .PHONY: clean
 
